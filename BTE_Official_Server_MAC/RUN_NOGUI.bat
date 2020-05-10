aac0865fba7358f2a10cd11cb68d3c73ce8c82bb
Tys vanZeyl <ttis3.14@gmail.com>
2020-05-09 21:21:49 -0700

    delete unnessesary files

--- BTE_Official_Server_MAC/RUN_NOGUI.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title 1.12.2 Terra Server
-java -XX:+UseG1GC -Xss4M -Xmx4G -Xms4G -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Duser.language=en -Duser.country=US -jar forge.jar nogui
-pause
--- BTE_Official_Server_MAC/run_nogui.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -XX:+UseG1GC -Xss4M -Xmx4G -Xms4G -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Duser.language=en -Duser.country=US -jar forge.jar nogui
