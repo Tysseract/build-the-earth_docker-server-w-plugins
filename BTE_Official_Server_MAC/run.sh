1b2c1f155ad9893fb20155d70330bc5160cb4f8d
Tys vanZeyl <ttis3.14@gmail.com>
2020-05-09 21:20:19 -0700

    run with docker enviroment variables passed to java

--- BTE_Official_Server_MAC/run.sh
+++ BTE_Official_Server_MAC/run.sh
@@ -6,4 +6,4 @@ fi
 echo "setting eula to" ${EULA} "based on enviroment variable (-e EULA="${EULA}")"
 echo "eula="${EULA} > /minecraft/eula.txt
 cat /minecraft/eula.txt
-java -XX:+UseG1GC -Xss4M -Xmx4G -Xms4G -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Duser.language=en -Duser.country=US -jar forge.jar
+java -XX:+UseG1GC -Xss${Xss} -Xmx${Xmx} -Xms${Xms} -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Duser.language=en -Duser.country=US -jar forge.jar
