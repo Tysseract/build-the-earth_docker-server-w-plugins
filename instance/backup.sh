cdaef27e3a6883b549e4893f9e0850a6fe0836af
Tys vanZeyl <ttis3.14@gmail.com>
2020-05-10 23:05:39 -0700

    using sleep

--- instance/backup.sh
+++ instance/backup.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-echo "Starting backup to remote"
-echo "Starting backup to remote" > /backups/log.txt
-cd /backups/Backup
-echo "removing old origin" >> /backups/log.txt
-git remote rm origin  || true
-echo "adding origin"${GITURL} >> /backups/log.txt
-git remote add origin ${GITURL} >> /backups/log.txt
-echo "pushing" >> /backups/log.txt
-git push origin -f --allow-unrelated-histories >> /backups/log.txt
+echo "waiting 5 minutes to start first backup to remote"
+  sleep 5m
+  echo "Starting backup to remote"
+  echo "Starting backup to remote" >> /backups/log.txt
+  cd /backups/Backup
+  echo "removing old origin" >> /backups/log.txt
+  git remote rm origin  || true
+  echo "adding origin"${GITURL} >> /backups/log.txt
+  git remote add origin ${GITURL} >> /backups/log.txt
+  echo "pushing"
+  git push origin -f --allow-unrelated-histories >> /backups/log.txt
+while [ true ]
+do
+  echo "waiting 15 minutes to push to remote"
+  sleep 15m
+  echo "pushing"
+  git push origin -f --allow-unrelated-histories >> /backups/log.txt
+done
--- instance/run.sh
+++ instance/run.sh
@@ -6,5 +6,9 @@ fi
 echo "setting eula to" ${EULA} "based on enviroment variable (-e EULA="${EULA}")"
 echo "eula="${EULA} > /minecraft/eula.txt
 cat /minecraft/eula.txt
-rc-service --list
+if [[ ! -z "$GITURL" ]]; then
+  echo "setting up git for backups..."
+  chmod +x /minecraft/backup.sh
+  ./minecraft/backup.sh &
+fi
 java -XX:+UseG1GC -Xss${Xss} -Xmx${Xmx} -Xms${Xms} -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Duser.language=en -Duser.country=US -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
